7e1b71aa6aafb9a1d6e076775eba8933fbafc6ae
AQIBNAZIR0081 <144234362+AQIBNAZIR0081@users.noreply.github.com>
2026-03-11 16:12:19 +0500

    create a cinemachine camera to follow the active target object in the scene

--- Assets/_Project/Scripts/FollowCamera.cs
+++ Assets/_Project/Scripts/FollowCamera.cs
@@ -1,18 +1,26 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    public Camera _camera;
-    public Vector3 offset; // The offset from the target position
-
+    public CinemachineCamera _camera;
+    public GameObject[] _targets;
 
     private void Start()
     {
-        _camera = Camera.main;
+        _camera = FindAnyObjectByType<CinemachineCamera>();
     }
 
     private void LateUpdate()
     {
-        _camera.transform.position = new Vector3 (0,0,transform.position.z) + offset;
+        for (int i = 0; i < _targets.Length; i++)
+        {
+            if (_targets[i] != null && _targets[i].activeInHierarchy)
+            {
+                _camera.Follow = _targets[i].transform;
+                _camera.LookAt = _targets[i].transform;
+                break;
+            }
+        }
     }
 }
